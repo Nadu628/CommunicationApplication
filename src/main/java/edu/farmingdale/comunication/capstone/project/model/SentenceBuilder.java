8107db1d97515c37094ab4666d2d631d6d99ba21
Joseph a Nunez <joseph.a.nunez22@gmail.com>
2025-04-01 12:31:38 -0400

    Second Commit. pom.xml is altered but needs to be adjusted to fit project criteria, sentence buiilder and imageTile Class fully functional.

--- src/main/java/edu/farmingdale/comunication/capstone/project/model/SentenceBuilder.java
+++ src/main/java/edu/farmingdale/comunication/capstone/project/model/SentenceBuilder.java
@@ -1,4 +1,19 @@
 package edu.farmingdale.comunication.capstone.project.model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class SentenceBuilder {
+    private List<String> words = new LinkedList<>();
+
+    public void addWord(String word) {
+        words.add(word);
+    }
+
+    public void removeLastWord() {
+        if (!words.isEmpty()) words.removeLast();
+    }
+    public String getSentence() {
+        return String.join(" ", words);
+    }
 }
--- src/main/java/edu/farmingdale/comunication/capstone/project/model/SpeechSynthesizer.java
+++ src/main/java/edu/farmingdale/comunication/capstone/project/model/SpeechSynthesizer.java
@@ -1,4 +1,16 @@
 package edu.farmingdale.comunication.capstone.project.model;
 
 public class SpeechSynthesizer {
+    private static final String API_KEY = "7ibYrSgXmO0LROJ0iQaKdAdYNgz8GxBa51E8hvMTmmwI3OqdfFEQJQQJ99BDACYeBjFXJ3w3AAAYACOGLYt5";
+    private static final String REGION = "eastus";
+
+    public static void speak(String text) {
+        try {
+            SpeechConfig config = SpeechConfig.fromSubscription(API_KEY, REGION);
+            SpeechSynthesizer synthesizer = new SpeechSynthesizer(config);
+            synthesizer.SpeakText(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
